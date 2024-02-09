7041ecc20d0cc30e31eb222bf7b38c3003c7fc58
leanne <156992399+ljw18@users.noreply.github.com>
2024-02-10 00:55:52 +1100

    Add Assignment 2 file
    Creating variables and strings

---
+++ Assignment 2.py
@@ -0,0 +1,14 @@
+original_text = "I'm so excited! "
+shouting_text = original_text.upper()
+annoying_text = 10*shouting_text
+annoying_text_characters = len(annoying_text)
+
+print(f"My annoying text is {annoying_text}\n\nIt has {annoying_text_characters} characters.")
+
+
+
+
+
+
+      
+
